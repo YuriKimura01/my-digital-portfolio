599ac1daea9cf940c45452e0058a69c6760689a0
YuriKimura01 <yuri.kimura0@gmail.com>
2025-05-30 20:47:34 +1000

    feat: connect project cms

---
+++ lib/api.ts
@@ -0,0 +1,10 @@
+export async function getProjects() {
+  console.log("Fetching on the server...");
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_CMS_URL}/api/projects?depth=1`
+  );
+
+  if (!res.ok) throw new Error("Failed to fetch projects");
+
+  return res.json();
+}
